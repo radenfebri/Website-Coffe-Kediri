caeab147068c4097f337ec529eb024c7256b54ff
alifian438 <alifian290126@gmail.com>
2022-11-23 14:54:46 +0700

    update kategori

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -570,26 +570,58 @@
     {{-- KATEGORI POPULER --}}
     <section class="popular-categories section-padding mt-15 mb-25">
         <div class="container wow fadeIn animated">
-            <h3 class="section-title mb-20"><span>Kategori</span> Populer</h3>
+            <h3 class="section-title mb-20"><span>Popular</span> Categories</h3>
             <div class="carausel-6-columns-cover position-relative">
                 <div class="slider-arrow slider-arrow-2 carausel-6-columns-arrow" id="carausel-6-columns-arrows"></div>
                 <div class="carausel-6-columns" id="carausel-6-columns">
-                    @if ($kategoriproduk->count() > 0)
-                    @foreach ($kategoriproduk as $item)
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            @if ($item->image == null)
-                            <a href="#"><img src="{{ '/frontend/imgs/shop/category-thumb-1.jpg' }}" loading="lazy" alt="{{ $item->name }}"></a>
-                            @else
-                            <a href="#"><img src="{{ asset('storage/'. $item->image ) }}" loading="lazy" alt="{{ $item->name }}"></a>
-                            @endif
+                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-1.jpg" alt=""></a>
                         </figure>
-                        <h5><a href="#">{{ $item->name }}</a></h5>
+                        <h5><a href="shop.html">T-Shirt</a></h5>
                     </div>
-                    @endforeach
-                    @else
-                    Data Kategori produk Populer Kosong
-                    @endif
+                    <div class="card-1">
+                        <figure class=" img-hover-scale overflow-hidden">
+                            <a href="shop.html"> <img src="assets/imgs/shop/category-thumb-2.jpg" alt=""></a>
+                        </figure>
+                        <h5><a href="shop.html">Bags</a></h5>
+                    </div>
+                    <div class="card-1">
+                        <figure class=" img-hover-scale overflow-hidden">
+                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-3.jpg" alt=""></a>
+                        </figure>
+                        <h5><a href="shop.html">Sandan</a></h5>
+                    </div>
+                    <div class="card-1">
+                        <figure class=" img-hover-scale overflow-hidden">
+                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-4.jpg" alt=""></a>
+                        </figure>
+                        <h5><a href="shop.html">Scarf Cap</a></h5>
+                    </div>
+                    <div class="card-1">
+                        <figure class=" img-hover-scale overflow-hidden">
+                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-5.jpg" alt=""></a>
+                        </figure>
+                        <h5><a href="shop.html">Shoes</a></h5>
+                    </div>
+                    <div class="card-1">
+                        <figure class=" img-hover-scale overflow-hidden">
+                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-6.jpg" alt=""></a>
+                        </figure>
+                        <h5><a href="shop.html">Pillowcase</a></h5>
+                    </div>
+                    <div class="card-1">
+                        <figure class=" img-hover-scale overflow-hidden">
+                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-7.jpg" alt=""></a>
+                        </figure>
+                        <h5><a href="shop.html">Jumpsuits</a></h5>
+                    </div>
+                    <div class="card-1">
+                        <figure class=" img-hover-scale overflow-hidden">
+                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-8.jpg" alt=""></a>
+                        </figure>
+                        <h5><a href="shop.html">Hats</a></h5>
+                    </div>
                 </div>
             </div>
         </div>
