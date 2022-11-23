8ee24a64fca3c5e97f709012b2c52a857963ae2e
radenfebri <febriye12@gmail.com>
2022-11-23 15:27:54 +0700

    Update Baru

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -570,58 +570,20 @@
     {{-- KATEGORI POPULER --}}
     <section class="popular-categories section-padding mt-15 mb-25">
         <div class="container wow fadeIn animated">
-            <h3 class="section-title mb-20"><span>Popular</span> Categories</h3>
+            <h3 class="section-title mb-20"><span>Kategori</span> Populer</h3>
             <div class="carausel-6-columns-cover position-relative">
                 <div class="slider-arrow slider-arrow-2 carausel-6-columns-arrow" id="carausel-6-columns-arrows"></div>
                 <div class="carausel-6-columns" id="carausel-6-columns">
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="{{ asset('frontend')}}/imgs/shop/category-thumb-1.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">T-Shirt</a></h5>
-                    </div>
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"> <img src="{{ asset('frontend')}}/imgs/shop/category-thumb-2.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">Bags</a></h5>
-                    </div>
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="{{ asset('frontend')}}/imgs/shop/category-thumb-3.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">Sandan</a></h5>
-                    </div>
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-4.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">Scarf Cap</a></h5>
-                    </div>
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-5.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">Shoes</a></h5>
-                    </div>
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-6.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">Pillowcase</a></h5>
-                    </div>
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-7.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">Jumpsuits</a></h5>
-                    </div>
-                    <div class="card-1">
-                        <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-8.jpg" alt=""></a>
-                        </figure>
-                        <h5><a href="shop.html">Hats</a></h5>
-                    </div>
+
+                    @foreach ($kategoriproduk as $item)
+                        <div class="card-1">
+                            <figure class=" img-hover-scale overflow-hidden">
+                                <a href=""> <img src="{{ asset('storage/' . $item->image )}}" alt="{{ $item->name }}"></a>
+                            </figure>
+                            <h5><a href="">{{ $item->name }}</a></h5>
+                        </div>
+                    @endforeach
+
                 </div>
             </div>
         </div>
