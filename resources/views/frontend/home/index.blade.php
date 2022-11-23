be396ff5e9a186cb97373636cc4bf3e40f055878
alifian438 <alifian290126@gmail.com>
2022-11-23 15:09:20 +0700

    update

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -576,49 +576,49 @@
                 <div class="carausel-6-columns" id="carausel-6-columns">
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-1.jpg" alt=""></a>
+                            <a href="shop.html"><img src="{{ asset('frontend')}}/imgs/shop/category-thumb-1.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">T-Shirt</a></h5>
                     </div>
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"> <img src="assets/imgs/shop/category-thumb-2.jpg" alt=""></a>
+                            <a href="shop.html"> <img src="{{ asset('frontend')}}/imgs/shop/category-thumb-2.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">Bags</a></h5>
                     </div>
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-3.jpg" alt=""></a>
+                            <a href="shop.html"><img src="{{ asset('frontend')}}/imgs/shop/category-thumb-3.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">Sandan</a></h5>
                     </div>
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-4.jpg" alt=""></a>
+                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-4.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">Scarf Cap</a></h5>
                     </div>
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-5.jpg" alt=""></a>
+                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-5.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">Shoes</a></h5>
                     </div>
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-6.jpg" alt=""></a>
+                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-6.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">Pillowcase</a></h5>
                     </div>
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-7.jpg" alt=""></a>
+                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-7.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">Jumpsuits</a></h5>
                     </div>
                     <div class="card-1">
                         <figure class=" img-hover-scale overflow-hidden">
-                            <a href="shop.html"><img src="assets/imgs/shop/category-thumb-8.jpg" alt=""></a>
+                            <a href="shop.html"><img src="{{ asset('frontend') }}/imgs/shop/category-thumb-8.jpg" alt=""></a>
                         </figure>
                         <h5><a href="shop.html">Hats</a></h5>
                     </div>
