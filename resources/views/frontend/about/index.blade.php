d553f82319af0eed4e1700d21a8495dee686b95e
alifian438 <alifian290126@gmail.com>
2022-12-01 12:40:20 +0700

    update kategori homme

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -133,35 +133,5 @@
             </div>
         </div>
     </section>
-    <section class="section-padding">
-        <div class="container pb-25">
-            <h3 class="section-title mb-20 wow fadeIn animated text-center"><span>Featured</span> Clients</h3>
-            <div class="carausel-6-columns-cover position-relative wow fadeIn animated">
-                <div class="carausel-6-columns text-center" id="carausel-6-columns-3">
-                    <div class="brand-logo">
-                        <img class="img-grey-hover" src="{{ asset('frontend') }}/imgs/banner/brand-1.png" alt="">
-                    </div>
-                    <div class="brand-logo">
-                        <img class="img-grey-hover" src="{{ asset('frontend') }}/imgs/banner/brand-2.png" alt="">
-                    </div>
-                    <div class="brand-logo">
-                        <img class="img-grey-hover" src="{{ asset('frontend') }}/imgs/banner/brand-3.png" alt="">
-                    </div>
-                    <div class="brand-logo">
-                        <img class="img-grey-hover" src="{{ asset('frontend') }}/imgs/banner/brand-4.png" alt="">
-                    </div>
-                    <div class="brand-logo">
-                        <img class="img-grey-hover" src="{{ asset('frontend') }}/imgs/banner/brand-5.png" alt="">
-                    </div>
-                    <div class="brand-logo">
-                        <img class="img-grey-hover" src="{{ asset('frontend') }}/imgs/banner/brand-6.png" alt="">
-                    </div>
-                    <div class="brand-logo">
-                        <img class="img-grey-hover" src="{{ asset('frontend') }}/imgs/banner/brand-3.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 </main>
 @endsection
