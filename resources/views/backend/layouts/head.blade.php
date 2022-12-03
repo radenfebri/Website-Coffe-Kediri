c9994f757bd4d2d4474da7e79bc6cb16edc6fef7
radenfebri <febriye12@gmail.com>
2022-12-03 15:13:40 +0700

    Push

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -75,8 +75,6 @@
     
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <!--  BEGIN CUSTOM STYLE FILE  -->
-    @vite([])
-    
     
     
 </head>
