e96a416c04283d398dccf99cf5ea547383abac5f
alifian438 <alifian290126@gmail.com>
2022-12-09 10:56:23 +0700

    update navbar

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -45,8 +45,8 @@
     </div>
     <div class="header-middle header-middle-ptb-1 d-none d-lg-block">
         <div class="container">
-            <div class="header-wrap">
-                <div class="logo logo-width-1">
+            <div class="header-wrap header-tengah">
+                <div class="logo logo-width-1 logo-header">
                     @if ($setting_website)
                     <a href="{{ route('home') }}"><img src="{{ asset('storage/' . $setting_website->image )}}" alt="logo"></a>
                     @else
@@ -82,50 +82,13 @@
                                     <span class="pro-count blue cart-count">0</span>
                                     @endguest
                                 </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="header-bottom header-bottom-bg-color sticky-bar">
-        <div class="container">
-            <div class="header-wrap header-space-between position-relative">
-                <div class="logo logo-width-1 d-block d-lg-none">
-                    @if ($setting_website)
-                    <a href="{{ route('home') }}"><img src="{{ asset('storage/' . $setting_website->image )}}" alt="logo"></a>
-                    @else
-                    
-                    @endif
-                </div>
-                <div class="header-nav d-none d-lg-flex">
-                    <div class="main-categori-wrap d-none d-lg-block">
-                        <a class="categori-button-active" href="#">
-                            <span class="fi-rs-apps"></span> Kategori Produk
-                        </a>
-                        
-                        <div class="categori-dropdown-wrap categori-dropdown-active-large">
-                            <ul>
-                                @foreach ($kategoriproduk_nav as $item)
-                                <li><a href="{{ route('kategori', $item->slug ) }}"><i class="surfsidemedia-font-home"></i>{{ $item->name }}</a></li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        
-                    </div>
-                    @guest
-                    <div class="main-menu main-menu-padding-1 main-menu-lh-2 d-none d-lg-block">
-                        <nav>
-                            <ul>
-                                <li><a class="{{ request()->is('/', 'home') ? 'active' : ''}}" href="{{ route('home') }}">Home </a></li>
-                                <li><a class="{{ request()->is('shop', 'shop/*', 'cart', 'favorit', 'detail-produk/*') ? 'active' : ''}}" href="{{ route('shop') }}">Shop</a></li>
-                                <li><a class="{{ request()->is('about', 'about/*') ? 'active' : ''}}" href="{{ route('about') }}">About</a></li>
-                                @guest
-                                
-                                @else
-                                <li><a href="#">My Account<i class="fi-rs-angle-down"></i></a>
-                                    <ul class="sub-menu">
+                            </div>                    
+                            <div class="header-action-icon-2">
+                                <a class="mini-cart-icon" href="cart.html">
+                                    <img alt="Surfside Media" src="{{ asset("frontend")}}/imgs/theme/icons/icon-user.svg">
+                                </a>
+                                <div class="cart-dropdown-wrap cart-dropdown-hm2 header-dropdown">
+                                    <ul>
                                         @guest
                                         
                                         @else
@@ -143,64 +106,65 @@
                                             @csrf
                                         </form>    
                                     </ul>
-                                </li>
-                                @endguest
-                            </ul>
-                        </nav>
-                    </div>                    
-                    
-                    @else
-                    <div class="main-menu navbar-login main-menu-padding-1 main-menu-lh-2 d-none d-lg-block">
-                        <nav>
-                            <ul>
-                                <li><a class="{{ request()->is('/', '') ? 'active' : ''}}" href="{{ route('home') }}">Home </a></li>
-                                <li><a class="{{ request()->is('shop', 'shop/*', 'detail-produk/*', 'checkout', 'favorit', 'cart') ? 'active' : ''}}" href="{{ route('shop') }}">Shop</a></li>
-                                <li><a class="{{ request()->is('about', 'about/*') ? 'active' : ''}}" href="{{ route('about') }}">About</a></li>
-                                <li><a class="{{ request()->is('order-history', 'setting', 'change-password') ? 'active' : ''}}" href="#">My Account<i class="fi-rs-angle-down"></i></a>
-                                    <ul class="sub-menu">
-                                        @guest
-                                        
-                                        @else
-                                        @if (Auth::user()->hasRole(['Super Admin', 'Admin']))
-                                        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                                        @else
-                                        
-                                        @endif
-                                        @endguest
-                                        <li><a href="{{ route('orderHistory') }}">Order History</a></li>
-                                        <li><a href="{{ route('setting') }}">Setting</a></li>
-                                        <li><a href="{{ route('changePassword') }}">Change Password</a> </li>   
-                                        <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>      
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>    
-                                    </ul>
-                                </li>
-                            </ul>
-                        </nav>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    @endguest
                 </div>
-                <div class="hotline d-none d-lg-block">
+            </div>
+        </div>
+    </div>
+    <div class="header-bottom header-bottom-bg-color sticky-bar">
+        <div class="container">
+            <div class="header-wrap header-space-between position-relative">
+                <div class="logo logo-width-1 d-block d-lg-none logo-header-mobile">
                     @if ($setting_website)
-                    <p><i class="fi-rs-smartphone"></i><span>Customer Service</span> {{ $setting_website->phone }} </p>
+                    <a href="{{ route('home') }}"><img src="{{ asset('storage/' . $setting_website->image )}}" alt="logo"></a>
                     @else
                     
                     @endif
                 </div>
+                <div class="header-nav d-none d-lg-flex header-bawah">
+                    <div class="main-categori-wrap d-none d-lg-block">
+                        <a class="categori-button-active" href="#">
+                            <span class="fi-rs-apps"></span> Kategori Produk
+                        </a>
+                        
+                        <div class="categori-dropdown-wrap categori-dropdown-active-large">
+                            <ul>
+                                @foreach ($kategoriproduk_nav as $item)
+                                <li><a href="{{ route('kategori', $item->slug ) }}"><i class="surfsidemedia-font-home"></i>{{ $item->name }}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="main-menu main-menu-padding-1 main-menu-lh-2 d-none d-lg-block">
+                        <nav>
+                            <ul>
+                                <li><a class="{{ request()->is('/', 'home') ? 'active' : ''}}" href="{{ route('home') }}">Beranda</a></li>
+                                <li><a class="{{ request()->is('shop', 'shop/*', 'cart', 'favorit', 'detail-produk/*') ? 'active' : ''}}" href="{{ route('shop') }}">Produk</a></li>
+                                <li><a class="{{ request()->is('about', 'about/*') ? 'active' : ''}}" href="{{ route('about') }}">Tentang Kami</a></li>
+                                @guest
+
+                                @else
+                                <li><a class="{{ request()->is('order-history', 'order-history/*') ? 'active' : ''}}" href="{{ route('orderHistory') }}">Riwayat Pesanan</a></li>
+                                @endguest
+                            </ul>
+                        </nav>
+                    </div>                            
+                    <div class="hotline d-none d-lg-block">
+                        @if ($setting_website)
+                        <p><a href="#"><i class="fi-rs-smartphone"></i><span>Customer Service</span> {{ $setting_website->phone }} </a></p>
+                        @else
+                        
+                        @endif
+                    </div>
+                </div>
                 <p class="mobile-promotion">Happy <span class="text-brand">Mother's Day</span>. Big Sale Up to 40%</p>
                 <div class="header-action-right d-block d-lg-none">
                     <div class="header-action-2">
-                        <div class="header-action-icon-2">
-                            <a href="{{ route('favorit.view') }}">
-                                <img alt="Surfside Media" src="{{ asset("frontend")}}/imgs/theme/icons/icon-heart.svg">
-                                @guest
-                                
-                                @else
-                                <span class="pro-count white wish-count">0</span>
-                                @endguest
-                            </a>
-                        </div>
                         <div class="header-action-icon-2">
                             <a class="mini-cart-icon" href="{{ route('cart') }}">
                                 <img alt="Surfside Media" src="{{ asset("frontend")}}/imgs/theme/icons/icon-cart.svg">
@@ -210,6 +174,20 @@
                                 <span class="pro-count white cart-count">0</span>
                                 @endguest
                             </a>
+                        </div>
+                        <div class="header-action-icon-2">
+                            <a class="mini-cart-icon" href="">
+                                <img alt="Surfside Media" src="{{ asset("frontend")}}/imgs/theme/icons/icon-user.svg">
+                            </a>
+                            <div class="cart-dropdown-wrap cart-dropdown-hm2">
+                                <div class="categori-dropdown-wrap categori-dropdown-active-large">
+                                    <ul>
+                                        @foreach ($kategoriproduk_nav as $item)
+                                        <li><a href="{{ route('kategori', $item->slug ) }}"><i class="surfsidemedia-font-home"></i>{{ $item->name }}</a></li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                         <div class="header-action-icon-2 d-block d-lg-none">
                             <div class="burger-icon burger-icon-white">
@@ -266,29 +244,12 @@
                 <!-- mobile menu start -->
                 <nav>
                     <ul class="mobile-menu">
-                        <li class="menu-item-has-children"><span class="menu-expand"></span><a href="{{ route('home') }}">Home</a></li>
-                        <li class="menu-item-has-children"><span class="menu-expand"></span><a href="{{ route('shop') }}">shop</a></li>
-                        <li class="menu-item-has-children"><span class="menu-expand"></span><a href="{{ route('about') }}">About</a></li>
+                        <li class="menu-item-has-children"><span class="menu-expand"></span><a href="{{ route('home') }}">Beranda</a></li>
+                        <li class="menu-item-has-children"><span class="menu-expand"></span><a href="{{ route('shop') }}">Produk</a></li>
+                        <li class="menu-item-has-children"><span class="menu-expand"></span><a href="{{ route('about') }}">Tentang Kami</a></li>
                         @guest
                         @else
-                        <li class="menu-item-has-children"><span class="menu-expand"></span><a href="#">My Account</a>
-                            <ul class="dropdown">
-                                @guest
-                                
-                                @endguest
-                                @if (Auth::user()->hasRole(['Super Admin', 'Admin']))
-                                <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                                @else
-                                @endif
-                                <li><a href="{{ route('orderHistory') }}">History Order</a></li>
-                                <li><a href="{{ route('setting') }}">Setting</a></li>
-                                <li><a href="{{ route('changePassword') }}">Change Password</a></li>
-                                <li><a href="{{ route('logout') }}"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>      
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>   
-                            </ul>
-                        </li>
+                        <li><a class="{{ request()->is('order-history', 'order-history/*') ? 'active' : ''}}" href="{{ route('orderHistory') }}">Riwayat Pesanan</a></li>
                         @endguest
                     </ul>
                 </nav>
